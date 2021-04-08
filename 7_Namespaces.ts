49abe06d754e1475537bad4923f68ee89c461d80
roman.maksimov001 <roman.maksimov001@gmail.com>
2021-04-08 14:09:46 +0300

    Namespaces

---
+++ 7_Namespaces.ts
@@ -0,0 +1,13 @@
+import {Utils} from "./Utils";
+
+const myPass = Utils.getPass('Roman', 21)
+const isSecret = Utils.isEmpty(Utils.SECRET)
+
+
+
+
+const PI = 3
+console.log(Utils.PI)
+console.log(PI)
+console.log(myPass)
+console.log(isSecret)
---
+++ Utils.ts
@@ -0,0 +1,8 @@
+export namespace Utils {
+    export const SECRET: string = '123432432'
+    export const PI: number = 3.14
+
+    export const getPass = (name: string, age: number): string => `${name} ${age}`
+
+    export const isEmpty = <T>(data: T): boolean => !data
+}
