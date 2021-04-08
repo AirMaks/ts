ba6f499fd754e43dbba72b1de7842f41196745fb
roman.maksimov001 <roman.maksimov001@gmail.com>
2021-04-08 14:13:05 +0300

    Modules

---
+++ 8_Modules.ts
@@ -0,0 +1,5 @@
+import {getPass, SECRET, PI} from "./Module";
+
+const myPass = getPass(SECRET, 21)
+
+console.log(myPass)
---
+++ Module.ts
@@ -0,0 +1,5 @@
+export const SECRET: string = '123432432'
+export const PI: number = 3.14
+
+export const getPass = (name: string, age: number): string => `${name} ${age}`
+
